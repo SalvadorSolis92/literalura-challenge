c9339350addd3d1ae764fcf7149aa5630f21f185
SalvadorSolis92 <angelcantu086@gmail.com>
2024-06-06 22:35:07 -0600

    Definición de opciones minimas en menu

--- literalura/src/main/java/com/aluracursos/literalura/literalura/principal/Principal.java
+++ literalura/src/main/java/com/aluracursos/literalura/literalura/principal/Principal.java
@@ -19,7 +19,11 @@ public class Principal {
         var opcion = -1;
         while (opcion != 0) {
             var menu = """
-                    1 - Buscar libro 
+                    1 - Buscar libro por titulo
+                    2 - Listar libros registrados
+                    3 - Listar autores registrados
+                    4 - Listar autores vivos en un año determinado
+                    5 - Listar libros por idioma
                     0 - Salir
                     
                     """;
